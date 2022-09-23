6ffc9a6dfa9c7ba77a2dd174dc41a6e8899cd64f
Carlos Blandon <blandoncarlos265@gmail.com>
2022-09-23 15:29:23 -0500

    Design of managament vierw created

--- pages/management.vue
+++ pages/management.vue
@@ -1,11 +1,40 @@
 <template>
-  <h1>Hello world</h1>
+  <v-row>
+    <v-col cols="12" class="primary d-flex justify-center">
+      <h1 v-text="viewTitle" class="white--text mt-3" />
+    </v-col>
+    <v-col cols="12" class="d-flex justify-center">
+      <div class="text-center">
+        <v-card
+          width="350"
+          height="350"
+          class="rounded-circle d-flex flex-row justify-center align-center mt-12 mb-15"
+        >
+          <h1 v-text="items.length" class="primary--text display-3" />
+        </v-card>
+
+        <v-btn class="primary" rounded>Gestionar</v-btn>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   layout: "empty",
+
+  data() {
+    return {
+      viewTitle: "Gestion base de datos",
+      items: [],
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.v-sheet.v-card:not(.v-sheet--outlined) {
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 5px 3px 0px rgb(17 120 100),
+    0px 1px 5px 0px rgb(0 0 0 / 12%);
+}
+</style>
