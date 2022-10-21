d4e573cafc068d9f6ab0aa4ace3464e5c6ee7a71
Carlos Blandon <blandoncarlos265@gmail.com>
2022-10-21 15:18:24 -0500

    The middleware only allowme browse in a view depends of rol

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-export default function ({ $api, redirect, store }) {
+export default function ({ $api, redirect, store, route }) {
   const token = store.state.localStorage.token;
 
   if (token === null) {
@@ -7,6 +7,20 @@ export default function ({ $api, redirect, store }) {
       name: "index",
     });
   } else {
+    const rol = store.state.localStorage.username.rol;
+    const nameRoute = route.name.split("-")[0];
+
+    if (nameRoute === "admin") {
+      if (rol !== "ADMINISTRADOR") {
+        redirect({ name: "index" });
+      }
+    }
+    if (nameRoute === "management" || nameRoute === "managementForm") {
+      if (rol.split(" ")[0] !== "BACKOFFICE") {
+        redirect({ name: "index" });
+      }
+    }
+
     $api.setHeader("Authorization", "Token " + token);
   }
 }
